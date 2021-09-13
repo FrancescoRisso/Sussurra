47004f2a4cafef153c2d21977dfbb4feae1e5a88
FrancescoRisso <francesco@frisso.net>
2021-09-13 12:37:55 +0200

    Version 1.0.2 First commit

---
+++ precache-manifest.9a2947752cf60089dd8b8c7ebe806573.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45c05f7532fe3f6d26494008e72773c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b4732f0d219c2afbd99",
+    "url": "/static/css/main.c608178a.chunk.css"
+  },
+  {
+    "revision": "856abcd1a9cdca951d7b",
+    "url": "/static/js/2.8ae112ac.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/static/js/2.8ae112ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b4732f0d219c2afbd99",
+    "url": "/static/js/main.fb2147e8.chunk.js"
+  },
+  {
+    "revision": "1647d47a3af171ec4f83",
+    "url": "/static/js/runtime-main.53d502f6.js"
+  },
+  {
+    "revision": "3982431efd69cd76b80f097ea9691d72",
+    "url": "/static/media/black_joker.3982431e.png"
+  },
+  {
+    "revision": "2328b55a26105ce1e97ac32ae2005178",
+    "url": "/static/media/dropdown.2328b55a.svg"
+  },
+  {
+    "revision": "bb3c015099ac0f2d4c7301c269247670",
+    "url": "/static/media/en.bb3c0150.svg"
+  },
+  {
+    "revision": "ff5324daad9749bb6fcf503f136199b1",
+    "url": "/static/media/it.ff5324da.svg"
+  },
+  {
+    "revision": "2160620598cf7da5c4760172d5c62794",
+    "url": "/static/media/man-icon-tick.21606205.svg"
+  },
+  {
+    "revision": "a01d06189b3e54033efb082c08ba2a25",
+    "url": "/static/media/man-icon.a01d0618.svg"
+  },
+  {
+    "revision": "6ed15038db8aea18dbfd534cef667a72",
+    "url": "/static/media/positioning.6ed15038.svg"
+  },
+  {
+    "revision": "119e0a23dc844a8c1ecb3b84595c3b4b",
+    "url": "/static/media/red_joker.119e0a23.png"
+  },
+  {
+    "revision": "91dd47d99c5f42db53e16aaca6d21375",
+    "url": "/static/media/sun-moon.91dd47d9.svg"
+  }
+]);
--- src/languagesList.js
+++
@@ -1,7 +0,0 @@
-import Ita from "./images/it.svg";
-import En from "./images/en.svg";
-
-export default [
-	{ code: "it", image: Ita, name: "Italiano" },
-	{ code: "en", image: En, name: "English" }
-];
